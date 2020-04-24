7f028ccd6382dae1d99e54fbc938255216b39c66
Mickael LY <mickael.ly.fr@gmail.com>
2020-04-24 10:00:14 +0200

    Add project files.

---
+++ TrackSmoother/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrackSmoother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
